1dd2c34b786d942433a83142ca59d1ec51fa7454
Nikita Levin <levin.nikitaa@gmail.com>
2026-03-04 18:33:21 +0200

    first commit

---
+++ src/components/Card.js
@@ -0,0 +1,28 @@
+import { Heading, HStack, Image, Text, VStack } from "@chakra-ui/react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
+import React from "react";
+
+const Card = ({ title, description, imageSrc }) => {
+  return (
+    <VStack
+      backgroundColor="white"
+      borderRadius="xl"
+      overflow="hidden"
+      alignItems="flex-start"
+      color="black"
+    >
+      <Image src={imageSrc} alt={title} width="100%" objectFit="cover" />
+      <VStack spacing={3} p={4} alignItems="flex-start">
+        <Heading as="h3" size="md">{title}</Heading>
+        <Text color="gray.600">{description}</Text>
+        <HStack spacing={2} color="black">
+          <Text fontWeight="bold">See more</Text>
+          <FontAwesomeIcon icon={faArrowRight} />
+        </HStack>
+      </VStack>
+    </VStack>
+  );
+};
+
+export default Card;
---
+++ src/components/LandingSection.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { Avatar, Heading, VStack } from "@chakra-ui/react";
+import FullScreenSection from "./FullScreenSection";
+import myPhoto from "../images/NikitaLevin.jpg";
+
+const greeting = "Hello, I am Nikita!";
+const bio1 = "A Full Stack developer";
+const bio2 = "specialised in Node.js, React and Python";
+
+
+const LandingSection = () => (
+  <FullScreenSection
+    justifyContent="center"
+    alignItems="center"
+    isDarkBackground
+    backgroundColor="#2A4365"
+  >
+    <VStack spacing={4}>
+      <Avatar boxSize="350px" src={myPhoto} />
+      <Heading as="h4" size="md">{greeting}</Heading>
+      <VStack spacing={1}>
+        <Heading as="h2" size="2xl">{bio1}</Heading>
+        <Heading as="h2" size="2xl">{bio2}</Heading>
+      </VStack>
+    </VStack>
+  </FullScreenSection>
+);
+
+export default LandingSection;
